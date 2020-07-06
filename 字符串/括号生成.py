a5b30cf83130b7a41755162df6927fcf32ab619d
许亮 <1357063398@qq.com>
2020-07-06 16:15:50 +0800

    add 括号生成

---
+++ 字符串/括号生成.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/7/6 16:15
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        ans = []
+        def backtrack(S, left, right):
+            if len(S) == 2 * n:
+                ans.append(''.join(S))
+            else:
+                if left < n:
+                    S.append('(')
+                    backtrack(S, left+1, right)
+                    S.pop()
+                if right < left:
+                    S.append(')')
+                    backtrack(S, left, right+1)
+                    S.pop()
+
+        backtrack([], 0, 0)
+        return ans
